fc04ef9b79b3815f06f99598accc6387b3e9ba7c
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-11-26 17:43:11 +0200

    Test Asynchronous Code (2) & Mocking ✅

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -2,16 +2,24 @@
 import Async from "./Async.jsx";
 
 describe('Async Component Test', () => {
-    test('renders posts if request succeeds', () => {
-        //! Arrange
+    test('renders posts if request succeeds', async () => {
+        //! Arrange (mock)
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{ id: 1, title: 'Test Post' }],
+        });
+
         render(<Async />);
         //! Act -> nothing to do
         //! Assert
-        const listItemElements = screen.getAllByRole('listitem') // https://www.w3.org/TR/html-aria/#docconformance
+        const listItemElements = await screen.findAllByRole('listitem') // https://www.w3.org/TR/html-aria/#docconformance
         expect(listItemElements).not.toHaveLength(0);
 
         //! The Test will fail
         //! why ? because screen.getAllByRole will instantly look for the listitem
         //! but it takes time to be here
+        //!
+        //! Solution: (find) returns a promise => will wait to the http request to succeed
+        //! the default timeout is 1 second
     })
 })
