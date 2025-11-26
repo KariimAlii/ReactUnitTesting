fed54c63e4ffe553ad23d312f84efb3a4a356007
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-11-26 17:30:32 +0200

    Test Asynchronous Code (1) & an issue ✅

---
+++ src/components/Async.test.js
@@ -0,0 +1,17 @@
+﻿import {render, screen} from "@testing-library/react";
+import Async from "./Async.jsx";
+
+describe('Async Component Test', () => {
+    test('renders posts if request succeeds', () => {
+        //! Arrange
+        render(<Async />);
+        //! Act -> nothing to do
+        //! Assert
+        const listItemElements = screen.getAllByRole('listitem') // https://www.w3.org/TR/html-aria/#docconformance
+        expect(listItemElements).not.toHaveLength(0);
+
+        //! The Test will fail
+        //! why ? because screen.getAllByRole will instantly look for the listitem
+        //! but it takes time to be here
+    })
+})
