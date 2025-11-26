1b6555c309556fa8dcd0158490be31e443dae435
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-11-26 16:15:03 +0200

    Testing User Interaction and State

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,6 +1,8 @@
 ﻿import {render, screen} from "@testing-library/react";
 import Greeting from "./Greeting.jsx";
 import '@testing-library/jest-dom';
+import userEvent from "@testing-library/user-event";
+
 
 
 //! A Test Suite is a group of tests combined together
@@ -30,5 +32,50 @@ describe('Greeting User Feature', () => {
         // const helloWorldElement = screen.queryByText('Hello World')
         // expect(helloWorldElement).not.toBeInTheDocument();
     })
+
+    test('Renders good to see you if the button was NOT Clicked', () => {
+        //! Arrange
+        render(<Greeting />)
+        //! Act --> nothing to do
+        //! Assert
+        const outputElement = screen.getByText('good to see you', {exact: false})
+        expect(outputElement).toBeInTheDocument();
+    })
+
+    test('Renders Changed! if the button was Clicked', async () => {
+        //! Arrange
+        render(<Greeting />)
+        //! Act => click the button
+        const buttonElement = screen.getByRole('button')
+        // userEvent.click(buttonElement)
+        const user = userEvent.setup();
+        await user.click(buttonElement);
+        //! Assert
+        const outputElement = screen.getByText('Changed', {exact: false})
+        expect(outputElement).toBeInTheDocument();
+    })
 })
 
+
+//! Notes : To integrate jest with your environment
+//!======================================================
+
+//! (1)Enable Jest in WebStorm
+//!==============================
+//! WebStorm → Settings → Languages & Frameworks → JavaScript → Libraries
+//! Click Download…
+//! Search for “jest”
+//! Install the Jest type definitions (@types/jest)
+
+//! (2) Install Jest type definitions
+//!=======================================
+//! npm install --save-dev @types/jest
+
+//! (3) Add Jest globals to ESLint
+//!===================================
+//! in (eslint.config.js)
+
+//! globals: {
+//!     ...globals.browser,
+//!     ...globals.jest
+//!  }
